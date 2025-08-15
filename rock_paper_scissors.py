d1cb57b74d9763f7d628a031e934e1db8f30353a
Jacob Hernandez <jacobhernandez928@gmail.com>
2025-08-14 17:50:46 -0700

    Rock Paper Scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,1 +1,33 @@
+import random
+choices = ["rock", "paper", "scissors"]
+playerScore = 0
+botScore = 0
+playerName = input("What is your name?")
 
+while True:
+  playerChoice = input("Welcome to Rock, Paper, Scissors! Enter a choice (or 'quit') to exit").lower()
+  if playerChoice == "quit":
+    break
+  if playerChoice not in choices:
+    print("Invalid selection. Try again.")
+    continue
+  computer = random.choice(choices)
+  print("Computer chose " + computer)
+
+  if playerChoice == computer:
+    print("It's a tie!")
+  elif playerChoice == "rock" and computer == "scissors" or playerChoice == "paper" and computer == "rock" or playerChoice == "scissors" and computer == "paper":
+    print("You win!")
+    playerScore += 1
+    print("Player: " + str(playerScore) + " - " + "Bot: " + str(botScore))
+  else:
+    print("Computer wins!")
+    botScore += 1
+    print("Player: " + str(playerScore) + " - " + "Bot: " + str(botScore))
+  
+print("Thanks for playing!")
+if playerScore > botScore:
+  print(playerName + " wins!")
+else:
+  print("Computer wins!")
+  
