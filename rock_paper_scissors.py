b165bf8ba21e66e63fb8f611d2b50740e0defaa9
Jacob Hernandez <jacobhernandez928@gmail.com>
2025-08-16 15:54:23 -0700

    Rock Paper Scissors
    Python program made to run a basic version of Rock Paper Scissors with score tracking.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,16 +1,20 @@
+# Imports and assigns all necessary variables in order for the game to run.
 import random
 choices = ["rock", "paper", "scissors"]
 playerScore = 0
 botScore = 0
-playerName = input("What is your name?")
+playerName = input("What is your name? ")
 
+# Loop used for game to run aslong as the player continues playing
 while True:
-  playerChoice = input("Welcome to Rock, Paper, Scissors! Enter a choice (or 'quit') to exit").lower()
+  # .lower() used to prevent errors within the program, turns all input lowercase
+  playerChoice = input("Welcome to Rock, Paper, Scissors! Enter a choice (or 'quit') to exit: ").lower()
   if playerChoice == "quit":
     break
   if playerChoice not in choices:
     print("Invalid selection. Try again.")
     continue
+  # Allows for computer to choose random choice out of the list
   computer = random.choice(choices)
   print("Computer chose " + computer)
 
@@ -19,15 +23,17 @@ while True:
   elif playerChoice == "rock" and computer == "scissors" or playerChoice == "paper" and computer == "rock" or playerChoice == "scissors" and computer == "paper":
     print("You win!")
     playerScore += 1
-    print("Player: " + str(playerScore) + " - " + "Bot: " + str(botScore))
-  else:
+    print("Player: " + str(playerScore) + " Bot: " + str(botScore))
+  else:                        
     print("Computer wins!")
     botScore += 1
-    print("Player: " + str(playerScore) + " - " + "Bot: " + str(botScore))
-  
+    print("Player: " + str(playerScore) +  " Bot: " + str(botScore))
+
+# Executes when the player decides to exit out of the loop
 print("Thanks for playing!")
 if playerScore > botScore:
   print(playerName + " wins!")
 else:
   print("Computer wins!")
   
+
